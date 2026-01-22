59c8b2dbd865ebc989b5b20ad500bed9038fb5a9
mingi-Shin <shinseogyong0621@gmail.com>
2026-01-22 16:07:36 +0900

    Initial commit

---
+++ src/test/java/com/mingisoft/gs/GongmosajaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mingisoft.gs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongmosajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
